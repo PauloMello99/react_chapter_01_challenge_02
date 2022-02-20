09cd2d4111f6ea1e742f3e4be7fdbea3a84fb7a8
Paulo Mello <paulovmello.99@outlook.com.br>
2022-02-20 15:37:33 -0300

    feat: Add challenge resolution

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { GenreResponseProps } from "../App";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  handleGenreClick: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ handleGenreClick, selectedGenreId }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleGenreClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
